134499116411fb859b9100c34272640b36b41885
2070super <1025404072@qq.com>
2023-01-07 21:04:46 +0800

    求 1+2+...+n ，要求不能使用乘除法、for、while、if、else、switch、case等关键字及条件判断语句（A?B:C）。

---
+++ 剑指 Offer 64. 求1+2+…+n/剑指 Offer 64. 求1+2+…+n/源.cpp
@@ -0,0 +1,34 @@
+#include<iostream>
+using namespace std;
+class sum
+{
+public:
+    sum()
+    {
+        _ret += _i;
+        ++_i;
+    }
+    static int getsum()
+    {
+        return _ret;
+    }
+private:
+    static int _i;
+    static int _ret;
+};
+int sum::_i = 1;
+int sum::_ret = 0;
+class Solution {
+public:
+    int sumNums(int n) {
+        sum*p=new sum[n];
+        return sum::getsum();
+    }
+};
+int main()
+{
+    int a;
+    cin >> a;
+    Solution s;
+    cout << s.sumNums(a) << endl;
+}
