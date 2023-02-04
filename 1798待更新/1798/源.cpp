5115eec230ea1f12037d433846dafeae81a2aceb
2070super <1025404072@qq.com>
2023-02-04 23:50:51 +0800

    待更新

---
+++ 1798待更新/1798/源.cpp
@@ -0,0 +1,19 @@
+
+#include<iostream>
+#include<algorithm>
+#include<vector>
+using namespace std;
+class Solution {
+public:
+    int getMaximumConsecutive(vector<int>& coins) {
+        sort(coins.begin(), coins.end());
+
+        int r = 1;
+        for (auto&& coin : coins) {
+            if (coin > r) { break; }
+            r += coin;
+        }
+
+        return r;
+    }
+};
